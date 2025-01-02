db5c3ad839759c154c7574f7f41a959cc1fad7ca
David Roberts <david@entendior.com>
2025-01-02 03:01:29 +0000

    Different way of check packages, more python, less bash.

---
+++ dev_resources/check_pip_2.py
@@ -0,0 +1,10 @@
+import importlib.metadata
+
+packages = ["aiolimiter", "python-dotenv", "openai", "opentelemetry-sdk", "opentelemetry-exporter-otlp-proto-grpc", "anyconfig", "pyyaml"]
+
+for package in packages:
+    try:
+         version = importlib.metadata.version(package)
+         print(f"{package}: {version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{package}: Not found")
