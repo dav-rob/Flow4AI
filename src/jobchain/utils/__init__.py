d5e531362104d676290c87d4b5550c74adf9ee23
David Roberts <david@entendior.com>
2025-01-23 00:00:27 +0000

    Added timing_decorator to init

--- src/jobchain/utils/__init__.py
+++ src/jobchain/utils/__init__.py
@@ -1,4 +1,5 @@
 # Make utils a Python package
 from .otel_wrapper import TracerFactory, trace_function
+from .timing import timing_decorator
 
-__all__ = ['TracerFactory', 'trace_function']
+__all__ = ['TracerFactory', 'trace_function', 'timing_decorator']
